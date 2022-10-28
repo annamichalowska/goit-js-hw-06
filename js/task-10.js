3e361120bd1236f0af9300697d0da081ce5a7463
Anna <michalowska.aneczka@gmail.com>
2022-10-28 21:52:59 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const boxesAmount = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", () => {});
+function createBoxes(amount) {
+  document.getElementById("#boxes").innerHTML = "Hello World";
+  //const addDiv = document.createElement("div");
+  //return addDiv;
+}
