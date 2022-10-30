6b0c20129107c5a389934f1d5d580f78c0993996
Anna <michalowska.aneczka@gmail.com>
2022-10-30 20:09:49 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,28 +1,32 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 const createBoxes = document.querySelector("button[data-create]");
 const destroyBoxes = document.querySelector("button[data-destroy]");
 const amount = document.querySelector("input");
 const startCreate = document.querySelector("#boxes");
-let inputValue = 0;
-let widthBox = 0;
-let heightBox = 0;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 createBoxes.addEventListener("click", () => {
-  inputValue = amount.value;
-  widthBox = 30;
-  heightBox = 30;
-  for (let i = 0; i < inputValue; i++) {
-    const box = document.createElement("div");
-    box.style.backgroundColor = getRandomHexColor();
-    box.style.width = widthBox + "px";
-    box.style.height = heightBox + "px";
-    startCreate.appendChild(box);
-    widthBox += 10;
-    heightBox += 10;
+  let inputValue = amount.value;
+  let widthBox = 30;
+  let heightBox = 30;
+  if (inputValue <= 100) {
+    for (let i = 0; i < inputValue; i++) {
+      const box = document.createElement("div");
+      box.style.backgroundColor = getRandomHexColor();
+      box.style.width = widthBox + "px";
+      box.style.height = heightBox + "px";
+      startCreate.appendChild(box);
+      widthBox += 10;
+      heightBox += 10;
+    }
+  } else {
+    alert("Maksymalna wartość to 100!");
   }
 });
+
 destroyBoxes.addEventListener("click", () => {
-  startCreate.remove();
+  startCreate.innerHTML = "";
+  amount.value = "";
 });
