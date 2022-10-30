2cb8571a6cd4d7a7092697b00e26cf1c1246b31e
Anna <michalowska.aneczka@gmail.com>
2022-10-30 12:13:48 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,8 @@
 const textSizeSlider = document.querySelector("input#font-size-control");
 const textToChangeSize = document.querySelector("span#text");
+
+textToChangeSize.style.fontSize = textSizeSlider.value + "px";
+
 textSizeSlider.addEventListener("input", (event) => {
   textToChangeSize.style.fontSize = event.target.value + "px";
 });
